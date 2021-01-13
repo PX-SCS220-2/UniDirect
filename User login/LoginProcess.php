1b6358af5d75d0e15bdcf6e77234327d4f03e949
klausdai1 <76425085+klausdai1@users.noreply.github.com>
2021-01-14 03:28:29 +1100

    Can login by using data from database

--- User login/LoginProcess.php
+++ User login/LoginProcess.php
@@ -1,20 +1,13 @@
 <?php
-        //get value passes from form in login.php
+//get value passes from form in login.php
     $username = $_POST['user'];
     $password = $_POST['pass'];
 
-    // to prevent mysql injection
-    $username = stripcslashes($username);
-    $password = stripcslashes($password);
-    $username = mysqli_real_escape_string($username);
-    $password = mysqli_real_escape_string($password);
-
     // connect to the server and select database
-    mysqli_connect("localhost", "root", "");
-    mysqli_select_db("login");
+    $con = mysqli_connect("127.0.0.1", "root", "", "login");
 
     //query the databse for user
-    $result = mysqli_query("select * from users where username = '$username' and password = '$password'")
+    $result = mysqli_query($con,"select * from users where username = '$username' and password = '$password'")
         or die("Failed to query database ".mysqli_error());
     $row = mysqli_fetch_array($result);
     if($row['username'] == $username && $row['password'] == $password){
