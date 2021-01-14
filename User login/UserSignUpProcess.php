29ae292ffcf49f26f28e6783b784cb68afb0f251
klausdai1 <76425085+klausdai1@users.noreply.github.com>
2021-01-14 17:20:12 +1100

    Making code more professional -Klaus

--- User login/UserSignUpProcess.php
+++ User login/UserSignUpProcess.php
@@ -1,33 +1,27 @@
+
 <?php
-session_start();
-
-$db = mysqli_connect("localhost", "root", "", "register");
-
-
-
-if(isset($_POST['register'])){
-
-    $username = mysqli_real_escape_string($_POST['user']);
-    $email = mysqli_real_escape_string($_POST['email']);
-    $password = mysqli_real_escape_string($_POST['pass']);
-    $password2 = mysqli_real_escape_string($_POST['pass2']);
+$server = "127.0.0.1";
+$user = "root";
+$pass = "";
+$db = "login";
 
 
-    if($password == $password2) {
-        $password = md5($password);
-        $sql = "INSERT INTO user(username, email, password) VALUES('$username', '$email', '$password')";
-        mysqli_query($db, $sql);
-        $_SESSION['message'] = "You are now success Sign up";
-        $_SESSION['username'] = $username;
-        header("location: login.php");
+$con = mysqli_connect($server, $user, $pass, $db);
 
-    }else{
-        $_SESSION['message'] = "The two password do not match";
-    }
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
 }
 
+$username = mysqli_real_escape_string($con,$_REQUEST['user']);
+$password = mysqli_real_escape_string($con,$_REQUEST['pass']);
+$email = mysqli_real_escape_string($con,$_REQUEST['email']);
 
-?>
+$insert = "INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES (NULL , '$username', '$password', '$email')";
 
+if(mysqli_query($con, $insert)){
+    echo "You have been successfully Sign In!";
+}else{
+    echo "Error";
+}
 
-
+mysqli_close($con);
