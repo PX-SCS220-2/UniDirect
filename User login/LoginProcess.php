ac60cf2e8b886b932f33ac349fed83b873505c65
AojuHan <55187580+AojuHan@users.noreply.github.com>
2021-01-18 10:30:27 +1100

    add logo image ---han

--- User login/LoginProcess.php
+++ User login/LoginProcess.php
@@ -10,14 +10,36 @@
     // connect to the server and select database
     $con = mysqli_connect($server, $user, $pass, $db);
 
-    //query the databse for user
-    $result = mysqli_query($con,"select * from user where username = '$username' and password = '$password'")
+    //query the database for user
+    $result = mysqli_query($con,"select * from users where username = '$username' and password = '$password'")
         or die("Failed to query database ".mysqli_error());
     $row = mysqli_fetch_array($result);
     if($row['username'] == $username && $row['password'] == $password){
-        echo"Login Success! Welcome.".$row['username'];
+        echo"Login Success! Welcome, ".$row['username'];
     }else{
         echo"Failed to login!";
     }
 
     mysqli_close($con);
+
+    ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Page</title>
+    <Link rel="stylesheet" type="text/css" href=""
+</head>
+<body>
+
+<div id="frm">
+    <h1>
+        Student Page
+    </h1>
+
+
+        <a href="../UniDirect/index.html">Back to Main page</a><br>
+
+</div>
+</body>
+
+</html>
