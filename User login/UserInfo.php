5737457497e311e50e22982701bab7679abeff91
AojuHan <55187580+AojuHan@users.noreply.github.com>
2021-01-18 11:00:21 +1100

    personal profile has been create for edit infor ---han

---
+++ User login/UserInfo.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student profile</title>
+    <Link rel="stylesheet" type="text/css" href=""
+</head>
+<body>
+<div class="container">
+    <form action="">
+
+        <label for="fname">First Name</label>
+        <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+        <label for="lname">Last Name</label>
+        <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+        <label for="education">Education</label>
+        <select id="education" name="education">
+            <option value="australia">Diploma</option>
+            <option value="canada">Bachelor</option>
+            <option value="usa">Master</option>
+            <option value="usa">Phd</option>
+        </select>
+
+        <label for="refer">Reference Letter</label>
+        <textarea id="refer" name="refer" placeholder="Write something.." style="height:200px"></textarea>
+
+        <input type="" value="Edit">
+
+    </form>
+</div>
