20361bcbe90d9ec7a6e1722631bbbded35122ac1
AojuHan <55187580+AojuHan@users.noreply.github.com>
2021-01-18 15:27:32 +1100

    student offer page, need more information  ---han

---
+++ User login/Offers.php
@@ -0,0 +1,23 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Offer Page</title>
+    <Link rel="stylesheet" type="text/css" href="Offers.css"
+</head>
+<img src="img/Logo.png" height="60">
+<body>
+
+    <h1>
+        Offer page
+    </h1>
+
+
+
+
+
+
+
+</body>
+</html>
