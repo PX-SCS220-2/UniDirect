fe1b6e0b8b9da31849bd794f5e5b5c3272e967c1
Adel2021 <63666689+Adel2021@users.noreply.github.com>
2021-01-19 14:47:12 +1100

    Adel--last one
    need to test

--- Admin login/forget-password.php
+++ Admin login/forget-password.php
@@ -1,4 +1,43 @@
-<?php // php interpret the PHP code ?>
+<?php
+
+require 'inc/admin_connection.php';
+require 'func/university-detail-reject-approve.php';
+require 'func/university-marked-Approve-reject.php';
+require 'func/add-university-form.php';
+require 'func/add-university.php';
+
+
+if (isset($_POST['forget-password'])) {
+
+
+    $username    = $_POST["username"];
+
+
+    $query  = "SELECT * FROM admin WHERE Admin_Username='$username' ";
+    $result = mysqli_query($admin_con, $query);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+
+           ?> <script> alert("Thankyou! Your password Successfully sent!");
+
+           window.location.replace("forget-password.php");
+   </script>
+<?php
+
+        }
+    }
+    else { ?>
+
+    	<script>
+    		alert("Sorry this email don't exists! Please try again!");
+    	     window.location.replace("forget-password.php");
+    	     </script>
+
+    	     <?php
+    	 }
+    	}
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,13 +45,17 @@
 	<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
 </head>
 <body>
+	<div class="container">
+		<div class="logo">
+			<img src="img/unidirect.png">
 			<div class="login-box">
 				<h1>Forget Password</h1>
-				<input type="email" name="password" id="password" placeholder="Enter your email" required>
-				<input type="submit" name="submit" value="Submit"  >
+				<form action="forget-password.php" method="post">
+				<input type="text" name="username" id="username" placeholder="Enter your username" required>
+				<input type="submit" name="forget-password" value="Submit"  >
 				</form>
 				<div class="link">
-				<a href="login.html">Login here</a>
+				<a href="admin_login.php">Login here</a>
 				</div>
 			</div>
 		</div>
