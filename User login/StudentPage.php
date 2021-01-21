03185e8f85eedda2e51ed57bd6175e1aee447d02
AojuHan <55187580+AojuHan@users.noreply.github.com>
2021-01-21 11:48:48 +1100

    need add functions about user page  ---han

--- User login/StudentPage.php
+++ User login/StudentPage.php
@@ -1,15 +1,42 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Page Title</title>
+    <title>User Page</title>
     <Link rel="stylesheet" type="text/css" href="StudentPageStyle.css">
 </head>
 <body>
  <img src="img/1001.PNG">
 
 
-<h1>This is a Heading</h1>
-<p>This is a paragraph.</p>
+<h1>This is a Student Page</h1>
+<p>When user log in, they should see this page.</p>
 
+
+
+ <div class="row">
+     <div class="col-md-4">
+         <table>
+             <tr>
+                 <th>
+                     <button type="button">Student Profile</button>
+                 </th>
+             </tr>
+             <tr>
+                 <th>
+                     <button type="button">Offers</button>
+                 </th>
+             </tr>
+             <tr>
+                 <th>
+                     <button type="button">Settings</button>
+                 </th>
+             </tr>
+             <tr>
+                 <th>
+                     <button type="button">Log Out</button>
+                 </th>
+             </tr>
+         </table>
+     </div>
 </body>
 </html>
