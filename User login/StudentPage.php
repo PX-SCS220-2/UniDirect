fdd3ee1a42f9617fb6d235afc2273cd240224178
AojuHan <55187580+AojuHan@users.noreply.github.com>
2021-01-21 14:05:32 +1100

    link each pages  ---han

--- User login/StudentPage.php
+++ User login/StudentPage.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,28 +13,36 @@
 <p>When user log in, they should see this page.</p>
 
 
-
  <div class="row">
      <div class="col-md-4">
          <table>
              <tr>
                  <th>
-                     <button type="button">Student Profile</button>
+                     <form method="get" action="UserInfo.php">
+                     <button onclick="window.location.href='/UserInfo.php'">Student Profile</button>
+                     </form>
                  </th>
              </tr>
              <tr>
                  <th>
-                     <button type="button">Offers</button>
+                     <form method="get" action="Offers.php">
+                         <button onclick="window.location.href='/Offers.php'">Offers</button>
+                     </form>
+
                  </th>
              </tr>
              <tr>
                  <th>
-                     <button type="button">Settings</button>
+                     <form method="get" action="#">
+                     <button onclick="window.location.href='#'">Settings</button>
+                     </form>
                  </th>
              </tr>
              <tr>
                  <th>
-                     <button type="button">Log Out</button>
+                     <form method="get" action="Logout.php">
+                     <button onclick="window.location.href='/Logout.php'">Log Out</button>
+                     </form>
                  </th>
              </tr>
          </table>
