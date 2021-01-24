dd22e250b09fba1996c0804efb6efd35d68698c0
AojuHan <55187580+AojuHan@users.noreply.github.com>
2021-01-25 10:58:52 +1100

    user register needs database

--- User login/UserSignUp.php
+++ User login/UserSignUp.php
@@ -13,29 +13,51 @@
     </h1>
     <form action="UserSignUpProcess.php" method="post">
 
-        <p>
-            <label>Username: </label>
-            <input type="text" name="user"  required/>
-        </p>
+        <div class="input-group">
+            <label class="textInput">User Name: </label>
+            <input type="text" name="firstname" placeholder="First Name">
+            <input type="text" name="lastname" placeholder="Last Name">
+        </div>
 
-        <p>
-            <label>Email: </label>
-            <input type="email"  name="email"  required/>
-        </p>
+            <div class="input-group">
+                <label class="textInput">Email: </label>
+                <input type="email"  name="email" placeholder="xxx@gmail.com" required/>
+            </div>
 
-        <p>
-            <label>Password: </label>
-            <input type="password" name="pass"  required/>
-        </p>
+        <div class="input-group">
+            <label class="textInput">Phone Number: </label>
+            <input type="text" name="phoneNumber"  required/>
+        </div>
 
-        <p>
-            <label>Confirm Password: </label>
-            <input type="password"  name="pass2"  required/>
-        </p>
 
-        <p>
+        <div class="input-group">
+            <label class="textInput">Address: </label>
+            <input type="text" name="address" />
+        </div>
+
+
+         <div class="input-group">
+             <label class="textInput">Password: </label>
+            <input type="password" name="password"  required/>
+         </div>
+
+        <div class="input-group">
+            <label class="textInput">Confirm Password: </label>
+            <input type="password"  name="password2"  required/>
+        </div>
+
+        <div class="input-group">
+        <label class="name">Select Your Account Type </label>
+        <select name="subject">
+            <option disabled="disabled" selected="selected">Account Type</option>
+            <option>Student</option>
+            <option>University</option>
+        </select>
+        </div>
+
+        <div>
             <input type="submit" name="register" value="Sign Up" />
-        </p>
+        </div>
 
             <p class="text-center">Already a Member? <a href="login.php">Sign In</a> </p>
 
