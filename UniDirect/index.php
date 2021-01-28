ee6a8410afc68154676fd63b1c1b9c8672018226
Adel2021 <63666689+Adel2021@users.noreply.github.com>
2021-01-28 18:40:49 +1100

    update index.html to index.php
    Because we add some functions we need to replace.html to .php

---
+++ UniDirect/index.php
@@ -0,0 +1,310 @@
+<?php require 'admin/inc/admin_connection.php'?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
+
+
+	<title>Home</title>
+	<style type="text/css">
+	</style>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+
+    <!-- Navbar Area -->
+    <?php require 'inc/nav.php'?>
+
+    <!-- Banner Area -->
+    <?php require 'inc/banner-search.php'?>
+
+
+    <div class="adverticement">
+
+
+	</div>
+
+	<div class="quote">
+		<p>
+			There are thousands of high-quality courses on offer in Australia, across a wide range of study areas. Courses range from vocational certificates and diplomas through to undergraduate degrees and postgraduate programs. <br> <br>
+
+			Here are some factors to keep in mind when choosing the course that is “right” for you; what you will learn, how you will be learning it, entry requirements, cost of study and what career path the course can lead you in to.
+		</p>
+	</div>
+
+
+
+	<div   class="col col-10 offset-1   slider-div">
+
+			
+		<u><h3>Not sure what you want to study?</h3></u>
+		
+
+		</div>
+	<div id="container">
+	 	<span id="prev" class="arrows-slider left-arrow-slider fa fa-arrow-left"></span>
+
+      <div id="slider">
+
+      	<div class='start slider-item'>
+        		<section style="background-image: url(img/005.jpg);">
+        			<h2>1</h2>
+        			<a class="btn btn-warning" href="#"><span class="fa fa-book"> Read More</span></a>
+        		</section>
+	     </div>
+
+	     <div class='start slider-item'>
+        		<section style="background-image: url(img/006.jpg);">
+        			<h2>2</h2>
+        			<a class="btn btn-warning" href="#"><span class="fa fa-book"> Read More</span></a>
+        		</section>
+	     </div>
+	      <div class='start slider-item'>
+        		<section style="background-image: url(img/007.jpg);">
+        			<h2>3</h2>
+        			<a class="btn btn-warning" href="#"><span class="fa fa-book"> Read More</span></a>
+        		</section>
+	     </div>
+	      <div class='start slider-item'>
+        		<section style="background-image: url(img/008.jpg);">
+        			<h2>4</h2>
+        			<a class="btn btn-warning" href="#"><span class="fa fa-book"> Read More</span></a>
+        		</section>
+	     </div>
+	      <div class='start slider-item'>
+        		<section style="background-image: url(img/009.jpg);">
+        			<h2>5</h2>
+        			<a class="btn btn-warning" href="#"><span class="fa fa-book"> Read More</span></a>
+        		</section>
+	     </div>
+	      <div class='start slider-item'>
+        		<section style="background-image: url(img/010.jpg);">
+        			<h2>6</h2>
+        			<a class="btn btn-warning" href="#"><span class="fa fa-book"> Read More</span></a>
+        		</section>
+	     </div>
+	      <div class='start slider-item'>
+        		<section style="background-image: url(img/011.jpg);">
+        			<h2>7</h2>
+        			<a class="btn btn-warning" href="#"><span class="fa fa-book"> Read More</span></a>
+        		</section>
+	     </div>
+	      <div class='start slider-item'>
+        		<section style="background-image: url(img/012.jpg);">
+        			<h2>8</h2>
+        			<a class="btn btn-warning" href="#"><span class="fa fa-book"> Read More</span></a>
+        		</section>
+	     </div>
+	 </div>
+	    <span id="next" class="arrows-slider right-arrow-slider fa fa-arrow-right"></span>
+
+	</div>
+
+<br>
+<br>
+<div class="col col-10 offset-1   slider-div-2">
+	<u><h3>Course tips, advice and news</h3></u>
+	<div class="slideshow-container">
+		<div class="mySlides fader">
+		  <img src="img/005.jpg" alt="img">
+		  <div class="text slider-caption"><h3><a href="">Is VET for you? 1</a ></h3>
+		  	<a href=""><b>Read More </b></a>
+		  </div>
+		</div>
+
+		<div class="mySlides fader">
+		  <img src="img/006.jpg" alt="img">
+		  <div class="text slider-caption"><h3><a href="">Is VET for you? 2</a ></h3>
+		  	<a href=""><b>Read More </b></a>
+		  </div>
+		</div>
+
+		<div class="mySlides fader">
+		  <img src="img/007.jpg" alt="img">
+		  <div class="text slider-caption"><h3><a href="">Is VET for you? 3</a></h3>
+		  	<a  href=""><b>Read More </b></a>
+		  </div>
+		</div>
+	</div>
+	<br>
+	<br>
+	<br>
+	<div class="dot-container" >
+	  <span class="dot" onclick="currentSlide(1)"></span> 
+	  <span class="dot" onclick="currentSlide(2)"></span> 
+	  <span class="dot" onclick="currentSlide(3)"></span> 
+	</div>
+
+
+</div>
+
+<div class="divider">
+
+</div>
+<br>
+<br>
+<br>
+<br>
+	<div class="footer">
+	<p>Copyright UniDirect 2020</p>
+	</div>
+
+
+</body>
+
+</html>
+
+
+
+
+
+
+
+<script type="text/javascript">$(function () { // Change navigation bar when scroll
+		$(window).scroll(function () {
+			var scroll = $(window).scrollTop();
+			if (scroll >= 30) {
+				$('.navbar').css('background-color', '#233367');
+			}
+			else {
+				$('.navbar').css('background-color', 'transparent');
+			}
+		});
+});</script>
+
+
+
+ <script>  //---------------------------------- SLIDER ROTATE------------------ //
+	$(window).load(function() {
+  	  var slider = ('#slider'),		//main division
+      imgs = $('#slider div'),		// all divisions inside that
+      sliderWidth = 0,
+      windowWidth = $(window).width(), // screen width
+      startImg,
+      startPos,
+      prev = $('#prev'),	// next prev button functions
+      next = $('#next'),	// next prev button functions
+      totalSlides = 4,
+      clickTotal = 0;
+
+	  for (var i = 0; i < imgs.length; i++) {
+	   sliderWidth += $(imgs[i]).outerWidth(); // get width all of images
+	  }
+		 imgs.filter(':first').addClass('start');
+		 startImg = $('.start').outerWidth();
+		 startPos = -((sliderWidth * 2) - (windowWidth / 2) + (startImg / 2)); // calculate starting position
+
+		 $('.slider-div').css('overflow', 'hidden'); // hide all images outside of main division
+
+		 $(slider).css('width', sliderWidth * 10); //make main division more larger
+
+		 imgs.filter(':first').before(imgs.clone());
+		 imgs.filter(':first').before(imgs.clone()); //Clone images first after last
+		 imgs.filter(':last').after(imgs.clone());
+
+		 $(slider).css('left', startPos); // get the position to the calculated start position
+
+
+		 prev.click(function(e) {  //Previous click
+		   e.preventDefault();
+		   clickTotal--;
+		   $(slider).animate({
+		     left: '+=' + sliderWidth / totalSlides * 2
+		   }, 1000, function() {
+		     if (clickTotal == -totalSlides) {
+		       clickTotal = 0;
+		       $(slider).css('left', startPos);
+		     }
+		   });
+		 });
+
+
+		 next.click(function(e) { //Next click
+		   e.preventDefault();
+		   clickTotal++;
+		   $(slider).animate({
+		     left: '-=' + sliderWidth / totalSlides * 2
+		   }, 1000, function() {
+		     if (clickTotal == totalSlides) {
+		       clickTotal = 0;
+		       $(slider).css('left', startPos);
+		     }
+		   });
+		 });
+
+		});
+</script>
+
+<script>// -------------------------------------- 2nd slider ---------------------//
+	var width =$(window).width();
+	let slideIndex;
+	if(width<768){		// If smalll screen
+		slideIndex = 1;
+		showSlides(slideIndex);
+	}
+	
+	$( window ).resize(function() { // when resize screen
+		width = $(window).width(); 
+		if(width<768){	// if screen is a mobile
+		slideIndex = 1;
+		showSlides(slideIndex);
+		}
+		else{	// if screen is large, division will devide as 3 parts
+			$('.mySlides').css('display','inline-block');
+			$('.mySlides').css('width','33%');
+
+		}
+
+	});
+	
+
+	function plusSlides(n) { 		
+	  showSlides(slideIndex += n);
+	}
+
+	function currentSlide(n) { 
+	  showSlides(slideIndex = n);
+	}
+
+	function showSlides(n) {
+	  var i;
+	  var slides = $(".mySlides");
+	  var dots = $(".dot");
+	  if (n > slides.length) {slideIndex = 1} 
+	  if (n < 1) {slideIndex = slides.length}
+	  for (i = 0; i < slides.length; i++) {
+	      slides[i].style.display = "none";  
+	  }
+	  for (i = 0; i < dots.length; i++) {
+	      dots[i].className = dots[i].className.replace(" active-dot", "");
+	  }
+	  slides[slideIndex-1].style.display = "block";  
+	  dots[slideIndex-1].className += " active-dot";
+	}
+
+</script>
+
+<script type="text/javascript"> 
+	$( window ).resize(function() { // when big screen size, slider should not 
+		var w = $(window).width(); 
+		if(w<768){ 
+            $("#slider").bind("mousewheel", function() {
+                return false;
+            });
+		}
+	};
+</script>
